0e98d58ca6fc9cf12f696db3006e3a2e686e09e3
Palam <serega217348@gmail.com>
2016-11-04 10:58:54 +0200

    Add project files.

---
+++ Drawer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Drawer.MvcApplication" Language="C#" %>
---
+++ Drawer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Drawer.Startup))]
+namespace Drawer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
