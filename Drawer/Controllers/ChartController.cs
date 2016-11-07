8cc30ac84e96a126b7f3ff0c88e9f4da975e3d50
Palam <serega217348@gmail.com>
2016-11-07 05:39:05 +0200

    Repaired begin-error and all parametres send to server

--- Drawer/Controllers/ChartController.cs
+++ Drawer/Controllers/ChartController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Routing;
 using System.Web.Http.Cors;
+using Drawer.Models;
 
 namespace Drawer.Controllers
 {
@@ -18,14 +19,16 @@ namespace Drawer.Controllers
         {
             try
             {
+                string[] listOfParams = id.Split(',');
                 List<int> list = new List<int>();
-                int count = int.Parse(id);
+                int count = int.Parse(listOfParams[0]);
                 Random rand = new Random();
                 for (int i = 0; i < count; i++)
                  {
                       list.Add(rand.Next(1, 99));
                  }
-                 return Request.CreateResponse(HttpStatusCode.OK, list); 
+                Pack pack = new Pack { array = list, type = listOfParams[1] };
+                 return Request.CreateResponse(HttpStatusCode.OK, pack); 
             }
             catch (Exception e)
             {
---
+++ Drawer/Models/Pack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Drawer.Models
+{
+    public class Pack
+    {
+        public List<int> array { get; set; }
+        public string type { get; set; }
+    }
+}
